e5aa6f385be95f956aeac61bebcb2283a35f5fab
szymek <szymon.fuerst@gmail.com>
2020-02-29 11:45:47 +0100

    zapiswanie obiektów do json

--- src/pl/sda/student/Main.java
+++ src/pl/sda/student/Main.java
@@ -15,10 +15,27 @@ public class Main {
 
         // serializacja - przekształcenie obiektu na formę tekstową
 
+        Student student1 = new Student("Piotr", "Nowak", 23);
+        Student student2 = new Student("Krzysztof", "Piechota", 54);
+        Student student3 = new Student("Jan", "Pietrucha", 78);
+        Student student4 = new Student("Kamil","Bulimia", 14);
+        Student student5 = new Student("Tomasz", "Kwiatkowski", 26);
+
+
         ObjectMapper objectMapper = new ObjectMapper();
 
         try {
             objectMapper.writeValue(new File("student.json"), student);
+            objectMapper.writeValue(new File("student1.json"), student1);
+            objectMapper.writeValue(new File("student2.json"), student2);
+            objectMapper.writeValue(new File("student3.json"), student3);
+            objectMapper.writeValue(new File("student4.json"), student4);
+            objectMapper.writeValue(new File("student5.json"), student5);
+
+
+
+
+
         } catch (IOException e) {
             e.printStackTrace();
         }
