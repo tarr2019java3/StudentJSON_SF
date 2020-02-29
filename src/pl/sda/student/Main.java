facee24b9c3c4c443637875b83457a1a092b114f
szymek <szymon.fuerst@gmail.com>
2020-02-29 11:36:00 +0100

    zapisywanie obiektu do json

--- src/pl/sda/student/Main.java
+++ src/pl/sda/student/Main.java
@@ -1,8 +1,26 @@
 package pl.sda.student;
 
+import com.fasterxml.jackson.databind.ObjectMapper;
+
+import java.io.File;
+import java.io.IOException;
+
 public class Main {
 
     public static void main(String[] args) {
-	// write your code here
+
+        Student student = new Student("Jan",
+                "Kowalski",
+                32);
+
+        // serializacja - przekształcenie obiektu na formę tekstową
+
+        ObjectMapper objectMapper = new ObjectMapper();
+
+        try {
+            objectMapper.writeValue(new File("student.json"), student);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
     }
 }
